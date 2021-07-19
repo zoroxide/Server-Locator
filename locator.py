20604e5441a50ec261c0ab433c04afd49f20c059
Loay Mohamed <72279810+loay-mohamed-xv@users.noreply.github.com>
2021-07-19 22:42:20 +0200

    Add files via upload

---
+++ locator.py
@@ -0,0 +1,19 @@
+from geoip import geolite2
+import socket
+
+hostname = input("Plz Enter the Website URL(www.site.com, without https): ")
+
+ip = socket.gethostbyname(hostname)
+locator = geolite2.lookup(ip)
+country = locator.country
+timezone = locator.timezone
+continent = locator.continent
+location = locator.location
+
+print("------------------------------------------")
+print(f"The website ip address: {ip}")
+print(f"Server Country: {country}")
+print(f"Server continent: {continent}")
+print(f"Server Timezone: {timezone}")
+print(f"Server Location on Map: {location}")
+print("------------------------------------------")
